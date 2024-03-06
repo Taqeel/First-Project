bfe0c09c0133d0b4d6add1a3948485067d6895ca
Taqeel <toka92akeel@gmail.com>
2024-03-06 18:34:25 +0300

    first up-date

---
+++ src/JavaClassPackage/JavaClass.java
@@ -0,0 +1,54 @@
+package JavaClassPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class JavaClass {
+	
+	WebDriver driver = new ChromeDriver();
+	String website = "https://www.google.com";
+	
+	@BeforeTest 
+	
+	public void myBeforTest() {
+		
+		
+		String myname = "Toqa Aqeel";
+		
+		System.out.println(myname.charAt(2));
+		System.out.println(myname.contains("m"));
+		
+		
+//		String website = "https://www.google.com";
+		
+		
+		
+		
+	}
+	
+	
+	@Test
+	
+	public void myfirstTest() {
+		
+		
+		driver.get(website.toUpperCase());
+		
+		
+		
+	}
+	
+	
+	@AfterTest
+	
+	public void myfirstAfterTest() {
+		
+		
+	}
+	
+	
+
+}
